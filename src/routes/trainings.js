d09a0988ceee22649a91bcd4fd1d6cdfc09a9de1
Rocio Rios <belenrios449@gmail.com>
2022-12-05 00:26:08 -0300

    error

--- src/routes/trainings.js
+++ src/routes/trainings.js
@@ -2,23 +2,23 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/finance', function(req, res, next) {
-    res.render('finanzas', { title: 'Express' });
+    res.render('finanzas', { title: 'Finanzas' });
 });
   
 router.get('/', function(req, res, next) {
-    res.render('capacitaciones', { title: 'Express' });
+    res.render('capacitaciones', { title: 'Capacitaciones' });
 });
   
-router.get('/guia-register', function(req, res, next) {
-    res.render('c_registrar', { title: 'Express' });
+router.get('/guide', function(req, res, next) {
+    res.render('c_registrar', { title: 'Gui de registro' });
 });
   
 router.get('/marketing', function(req, res, next) {
-    res.render('marketing', { title: 'Express' });
+    res.render('marketing', { title: 'Marketing' });
 });
 
 router.get('/tips', function(req, res, next) {
-    res.render('tips', { title: 'Express' });
+    res.render('tips', { title: 'Tips' });
 });
 
 module.exports = router
